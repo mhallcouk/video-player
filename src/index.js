7e0635d9c6d564395489beb5f6c9d60385ea1018
Mathew Hall <mhmathewhall@gmail.com>
2017-05-14 14:28:12 +0100

    Search Bar component added to project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import SearchBar from "./components/search_bar";
 //Create component that produces some html
 
 const API_KEY = "AIzaSyA75wHqQR6JWceOWQZwNoAGCldUyF0hGiU";
 
 
 const App = () => {
-  return <div>Hi!</div>;
+  return <div>
+    <SearchBar />
+  </div>;
 }
 
 
