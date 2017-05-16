6738d3f9e14e01d455d6f914fdbcd47ecd542326
Mathew Hall <mhmathewhall@gmail.com>
2017-05-16 10:52:48 +0100

    Controlled component and initial value added to the searchbar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,12 +4,14 @@ class SearchBar extends Component {
   constructor(props){
     super(props);
 
-    this.state = { term: "" };
+    this.state = { term: "Initial value" };
   }
   render() {
     return (
       <div>
-        <input onChange={event => this.setState({ term: event.target.value })}/>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}/>
         <p>Value of the input: {this.state.term}</p>
       </div>
     );
