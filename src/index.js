a9c3b3b0b9d490b8e012b627384f7f835f9dc024
Mathew Hall <mhmathewhall@gmail.com>
2017-05-26 11:42:08 +0100

    Youtube search request successful

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/search_bar";
 //Create component that produces some html
 
 const API_KEY = "AIzaSyA75wHqQR6JWceOWQZwNoAGCldUyF0hGiU";
 
+YTSearch({key: API_KEY, term: "surfboards"}, function(data) {
+  console.log(data);
+});
 
 const App = () => {
   return (
