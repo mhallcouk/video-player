74af586e70d46bd9e2deeea5ee24bc01d86ceaa4
Mathew Hall <mhmathewhall@gmail.com>
2017-06-04 17:43:32 +0100

    Stylesheet linked

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import './../style/style.css';
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
