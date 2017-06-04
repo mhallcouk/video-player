0765325e77c55c0bdfbd39e3c1aefad5a7b7a426
Mathew Hall <mhmathewhall@gmail.com>
2017-06-04 23:35:22 +0100

    Placeholder and padding added

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,6 +10,7 @@ class SearchBar extends Component {
     return (
       <div className="search-bar">
         <input
+          placeholder="Search"
           value={this.state.term}
           onChange={event => this.onInputChange(event.target.value)}/>
       </div>
